ace9faf68c32a2d3e668634900dac3238c53a511
Yuto Mori <y.mori@ist.hokudai.ac.jp>
2023-08-18 10:03:13 +0900

    install langchain and openai

--- api/src/main.py
+++ api/src/main.py
@@ -7,9 +7,11 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
+    print("Hello World")
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+    
