46f49dea5e334b67f3927ebca0cdfab9ca82682d
Yuto Mori <y.mori@ist.hokudai.ac.jp>
2023-08-18 13:17:11 +0900

    Output the amount to log

--- api/src/main.py
+++ api/src/main.py
@@ -1,12 +1,13 @@
-from typing import Union
+import logging
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from langchain.callbacks import get_openai_callback
+from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
 from langchain.output_parsers import CommaSeparatedListOutputParser
 from langchain.prompts import PromptTemplate
 from langchain.schema import HumanMessage, SystemMessage
-from langchain.chains import LLMChain
 
 app = FastAPI()
 
@@ -38,6 +39,8 @@ def query(text: str) -> dict:
 
 @app.post("/suggest")
 def suggest(selected_text: str="訓練", sentence: str="ITの利活用には、__が必要です。", n:int=3):
+    logger = logging.getLogger('uvicorn')
+    
     # 穴埋め問題を解く
     chat = ChatOpenAI(model_name="gpt-3.5-turbo")
     output_parser = CommaSeparatedListOutputParser()
@@ -47,12 +50,14 @@ def suggest(selected_text: str="訓練", sentence: str="ITの利活用には、_
         input_variables=["n", "sentence", "selected_text"],
         partial_variables={"format_instructions": format_instructions}
     )
-    chain = LLMChain(llm=chat, prompt=prompt, )
-    results = chain.run({"n": n, "sentence": sentence, "selected_text": selected_text})
+    chain = LLMChain(llm=chat, prompt=prompt)
+    with get_openai_callback() as cb:
+        results = chain.run({"n": n, "sentence": sentence, "selected_text": selected_text})
+        logger.info(cb)
     print(results)
 
     # 穴埋め後の文章を作成
-    suggest_sentence_list = [""] * n
+    suggest_sentence_list = [""] * n    
     result_list = [x.strip() for x in results.split(',')]
     for i in range(n):
         suggest_sentence_list[i] = sentence.replace("__", result_list[i])
