c9737340c02e622fa79615a4aac730fa78712d06
Yuto Mori <y.mori@ist.hokudai.ac.jp>
2023-08-18 18:11:15 +0900

    Output format controlled by prompt

--- api/src/main.py
+++ api/src/main.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -22,7 +23,7 @@ app.add_middleware(
 )
 
 class QueryBody(BaseModel):
-    text:str
+    text: str
 
 @app.get("/")
 def read_root():
@@ -42,29 +43,54 @@ def query(body: QueryBody) -> dict:
     logger.info(result)
     return {"output": result.content}
 
-@app.get("/suggest")
-def suggest(selected_text: str="訓練", sentence: str="ITの利活用には、__が必要です。", n:int=3):
+class SuggestBody(BaseModel):
+    selected_text: str = "興味深い内容だった"
+    draft: str = "本公演は生成AIがトピックである。内容は非常に__。次回も参加したい。"
+    n: int = 3
+
+@app.post("/suggest")
+def suggest(body: SuggestBody) -> dict:
     logger = logging.getLogger('uvicorn')
     
     # 穴埋め問題を解く
     chat = ChatOpenAI(model_name="gpt-3.5-turbo")
     output_parser = CommaSeparatedListOutputParser()
     format_instructions = output_parser.get_format_instructions()
+    input_template = """
+        {draft}
+        上記の文章の__の部分に入る言葉を{n}個出力してください。
+        出力は{selected_text}の類語が望ましい。
+        {format_instructions}
+    """
     prompt = PromptTemplate(
-        template="__の部分に{selected_text}に代わる言葉を{n}個出力してください。 {sentence}\n{format_instructions}",
-        input_variables=["n", "sentence", "selected_text"],
+        template=input_template,
+        input_variables=["n", "draft", "selected_text"],
         partial_variables={"format_instructions": format_instructions}
     )
     chain = LLMChain(llm=chat, prompt=prompt)
     with get_openai_callback() as cb:
-        results = chain.run({"n": n, "sentence": sentence, "selected_text": selected_text})
+        results = chain.run({"n": body.n, "draft": body.draft, "selected_text": body.selected_text})
         logger.info(cb)
-    print(results)
+    logger.info("chain run:")
+    result_list = output_parser.parse(results)
+    logger.info(result_list)
+
+    # draftの文章から__が含まれる文章を抽出
+    sentences = body.draft.replace("。", "。\n").split("\n")
+    sentences = [s.strip() for s in sentences if s.strip() != ""]
+    logger.info("sentences:")
+    logger.info(sentences)
+    part_draft = ""
+    for sentence in sentences:
+        if "__" in sentence:
+            part_draft = sentence
+            break
 
     # 穴埋め後の文章を作成
-    suggest_sentence_list = [""] * n    
-    result_list = [x.strip() for x in results.split(',')]
-    for i in range(n):
-        suggest_sentence_list[i] = sentence.replace("__", result_list[i])
+    suggest_sentence_list = [""] * body.n
+    for i in range(body.n):
+        suggest_sentence_list[i] = part_draft.replace("__", result_list[i])
+    logger.info("suggest_sentence_list:")
+    logger.info(suggest_sentence_list)
     return {"suggest":results, "suggest_sentence": suggest_sentence_list}
     
