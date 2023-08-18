eab0ca5ac7346a65fdda3da4ab29d0b5c58af560
Yuto Mori <y.mori@ist.hokudai.ac.jp>
2023-08-18 12:14:54 +0900

    output suggest word

--- api/src/main.py
+++ api/src/main.py
@@ -3,11 +3,14 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from langchain.chat_models import ChatOpenAI
+from langchain.output_parsers import CommaSeparatedListOutputParser
+from langchain.prompts import PromptTemplate
 from langchain.schema import HumanMessage, SystemMessage
+from langchain.chains import LLMChain
 
 app = FastAPI()
 
-origins = ["http://localhost:5173"]
+origins = ["http://localhost:50018"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -15,7 +18,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 @app.get("/")
 def read_root():
@@ -34,7 +36,18 @@ def query(text: str) -> dict:
     print(result)
     return {"output": result.content}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/suggest")
+def suggest(selected_text: str="訓練", sentence: str="ITの利活用には、__が必要です。", n:int=3):
+    chat = ChatOpenAI(model_name="gpt-3.5-turbo")
+    output_parser = CommaSeparatedListOutputParser()
+    format_instructions = output_parser.get_format_instructions()
+    prompt = PromptTemplate(
+        template="__に入る言葉を{n}個出力してください。{sentence}\n{format_instructions}",
+        input_variables=["n", "sentence"],
+        partial_variables={"format_instructions": format_instructions}
+    )
+    chain = LLMChain(llm=chat, prompt=prompt, )
+    results = chain.run({"n": n, "sentence": sentence})
+    print(results)
+    return {"suggest":results}
     
